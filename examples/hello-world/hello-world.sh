e40f0acece3ca574360cbb55c8e35c4d74d6d182
MrErlison <sisal.cordame_0a@icloud.com>
2022-02-27 10:50:39 -0300

    Ajustado tamanho da linha

--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,17 +1,19 @@
-# Para rodar o programa, coloque o c&oacute;digo no arquivo `ola-mundo.go` 
-# e use `go run`.
+# Para rodar o programa, coloque o c&oacute;digo no arquivo 
+# `ola-mundo.go` e use `go run`.
 $ go run ola-mundo.go
 Olá mundo
 
-# &Agrave;s vezes, queremos transformar nosso c&oacute;digo fonte
-# em programa execut&aacute;vel. Podemos fazer isso usando `go build`.
+# &Agrave;s vezes, queremos transformar nosso c&oacute;digo 
+# fonte em um programa execut&aacute;vel. Podemos fazer isso 
+# usando `go build`.
 $ go build ola-mundo.go
 $ ls
-ola-mundo	ola-mundo.go
+ola-mundo   ola-mundo.go
 
 # Agora, n&oacute;s podemos executar o programa diretamente.
 $ ./ola-mundo
 Olá mundo
 
-# Agora que podemos executar e construir progamas b&aacute;sicos 
-# em Go, vamos aprender mais sobre a linguagem.
+# Agora que podemos executar e construir progamas 
+# b&aacute;sicos em Go, vamos aprender mais sobre 
+# a linguagem.
