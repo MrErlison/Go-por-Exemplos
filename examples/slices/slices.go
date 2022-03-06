bff47b33c9f171fcbd9309707bb77461328c462e
johndoesantos <sisal.cordame_0a@icloud.com>
2022-03-06 19:48:40 -0300

    Ajuste e remoção do texto original

--- examples/slices/slices.go
+++ examples/slices/slices.go
@@ -1,6 +1,6 @@
 // _Slices_ são um tipo de dados no Go, dando uma
-// `interface` mais poderosa para sequências do 
-// que `array`.
+// _interface_ mais poderosa para sequências do 
+// que _array_.
 
 package main
 
@@ -8,76 +8,68 @@ import "fmt"
 
 func main() {
 
-	// Ao contrário dos `arrays`, os `slices` tem seus
+	// Ao contrário dos _arrays_, os _slices_ tem seus
 	// tipos apenas pelos elementos que contêm (não pelo
-	// número de elementos). Para criar um `slice` vazio
+	// número de elementos). Para criar um _slice_ vazio
 	// com comprimento diferente de zero, use construtor
-	// `make`. Aqui fazemos os `slices` de `string` de 
+	// _make_. Aqui fazemos os _slices_ de _string_ de 
 	// comprimento 3 (inicialmente com valor zero).
-
 	s := make([]string, 3)
 	fmt.Println("emp:", s)
 
-	// Podemos definir e obter exatamente como o `array`.
+	// Podemos definir e obter exatamente como o _array_.
 	s[0] = "a"
 	s[1] = "b"
 	s[2] = "c"
 	fmt.Println("seet", s)
 	fmt.Println("get:", s[2])
 
-	// `len` retorna o tamanho do `slice`.
+	// _len_ retorna o tamanho do _slice_.
 	fmt.Println("len:", len(s))
 
-	// In addition to these basic operations, slices
-	// support several more that make them richer than
-	// arrays. One is the builtin `append`, which
-	// returns a slice containing one or more new values.
-	// Note that we need to accept a return value from
-	// `append` as we may get a new slice value.
-	
-	// Além dessas operações básicas, os `slices` 
+	// Além dessas operações básicas, os _slices_
 	// suportam muito mais, o que os tornam mais
-	// rico do que o `array`. 
-	// Um deles é o `append`, que retorna um `slice` 
+	// rico do que o _array_. 
+
+	// Um deles é o _append_, que retorna um _slice_
 	// contendo um ou mais novos valores. Observe que
 	// precisamos aceitar um valor de retorno do 
-	// `append`, bem como podemos obter um novo valor
-	// do `slice`.
-
+	// _append_, bem como podemos obter um novo valor
+	// do _slice_.
 	s = append(s, "d")
 	s = append(s, "e", "f")
 	fmt.Println("apd:", s)
 
-	// `Sllices` também podem ser copiadas. Aqui criamos
-	// um `slice` vazio `c` do mesmo comprimento que `s` 
-	// e copiamos para `c` o `s`.
+	// _Slices_ também podem ser copiadas. Aqui criamos
+	// um _slice_ vazio _c_ do mesmo comprimento que _s_
+	// e copiamos para _c_ o _s_.
 	c := make([]string, len(s))
 	copy(c, s)
 	fmt.Println("cpy:", c)
 
-	// `Slices` suportam um operador “slice” com a 
-	// sintaxe `slice[low:high]`. Por exemplo, isso obtém
-	// um `slice` dos elementos s[2], s[3] e s[4].
+	// _Slices_ suportam um operador _slice_ com a 
+	// sintaxe _slice[low:high]_. Por exemplo, isso obtém
+	// um _slice_ dos elementos s[2], s[3] e s[4].
 	l := s[2:5]
 	fmt.Println("sl1:", l)
 
-	// Esse `slice` corta até (excluindo) `s[5]`.
+	// Esse _slice_ corta até (excluindo) _s[5]_.
 	l = s[:5]
 	fmt.Println("sl2:", l)
 
-	// E esse `slice` parte de (incluindo) `s[2]`.
+	// E esse _slice_ parte de (incluindo) _s[2]_.
 	l = s[2:]
 	fmt.Println("sl3:", l)
 
 	// Também podemos declarar e inicializar uma variável
-	// `slice` em uma única linha.
+	// _slice_ em uma única linha.
 	t := []string{"g", "h", "i"}
 	fmt.Println("dcl:", t)
 
-	// `Slices` podem ser compostos em estruturas de 
+	// _Slices_ podem ser compostos em estruturas de 
 	// dados multidimensionais. O comprimento dos 
-	// `slices` internos pode variar, ao contrário dos
-	// `array` multidimensionais.
+	// _slices_ internos pode variar, ao contrário dos
+	// _array_ multidimensionais.
 	twoD := make([][]int, 3)
 	for i := 0; i < 3; i++ {
 		innerLen := i + 1
