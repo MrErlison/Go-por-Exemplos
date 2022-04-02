c8eb5868ef3c095fa3120c80905b9a67847a7828
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-04-02 14:23:45 -0300

    Traduzido interfaces

--- examples/interfaces/interfaces.go
+++ examples/interfaces/interfaces.go
@@ -1,5 +1,5 @@
-// _Interfaces_ are named collections of method
-// signatures.
+// _Interfaces_ são chamadas de coleções de assinaturas
+// de métodos
 
 package main
 
@@ -8,57 +8,56 @@ import (
 	"math"
 )
 
-// Here's a basic interface for geometric shapes.
-type geometry interface {
+// Aqui está uma interface básica para formas geométricas
+type geometria interface {
 	area() float64
-	perim() float64
+	perimetro() float64
 }
 
-// For our example we'll implement this interface on
-// `rect` and `circle` types.
-type rect struct {
-	width, height float64
+// Para o nosso exemplo, implementaremos essa interface
+// nos tipos de `retangulo` e `circulo`.
+type retangulo struct {
+	largura, altura float64
 }
-type circle struct {
-	radius float64
-}
-
-// To implement an interface in Go, we just need to
-// implement all the methods in the interface. Here we
-// implement `geometry` on `rect`s.
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
+type circulo struct {
+	raio float64
 }
 
-// The implementation for `circle`s.
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
+// Para implementar uma interface no Go, só precisamos
+// implementar todos os métodos na interface. Aqui
+// implementamos `geometria` em `retangulos`.
+func (r retangulo) area() float64 {
+	return r.largura * r.altura
 }
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
+func (r retangulo) perimetro() float64 {
+	return 2*r.largura + 2*r.altura
 }
 
-// If a variable has an interface type, then we can call
-// methods that are in the named interface. Here's a
-// generic `measure` function taking advantage of this
-// to work on any `geometry`.
-func measure(g geometry) {
+// A implementação para `circulos`.
+func (c circulo) area() float64 {
+	return math.Pi * c.raio * c.raio
+}
+func (c circulo) perimetro() float64 {
+	return 2 * math.Pi * c.raio
+}
+
+// Se uma variável tiver um tipo de interface, podemos 
+// chamar os métodos que estão na interface nomeada. Aqui
+// está uma função genérica de `medida` que aproveita 
+// isso para trabalhar em qualquer `geometria`.
+func medida(g geometria) {
 	fmt.Println(g)
 	fmt.Println(g.area())
-	fmt.Println(g.perim())
+	fmt.Println(g.perimetro())
 }
 
 func main() {
-	r := rect{width: 3, height: 4}
-	c := circle{radius: 5}
+	r := retangulo{largura: 3, altura: 4}
+	c := circulo{raio: 5}
 
-	// The `circle` and `rect` struct types both
-	// implement the `geometry` interface so we can use
-	// instances of
-	// these structs as arguments to `measure`.
-	measure(r)
-	measure(c)
+	// Os tipos de estrutura `circulo` e `retangulos` implementam
+	// a interface de `geometria` para que possamos usar instâncias
+	// dessas estruturas como argumentos para `medida`.
+	medida(r)
+	medida(c)
 }
