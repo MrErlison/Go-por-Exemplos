4ebeae80b69229426399a238e02ac1942edf1bc8
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-04-22 10:05:07 -0300

    Traduzido timers

---
+++ examples/temporizador/temporizador.sh
@@ -0,0 +1,6 @@
+// O primeiro temporizador dispara ~2s depois que 
+// iniciarmos o programa, mas o segundo deve ser
+// interrompido antes que tenha a chance de disparar.
+$ go run temporizador.go
+Timer 1 disparou
+Timer 2 parou
