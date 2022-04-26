08ea0b6186209d0b7f292e45ae8def7cf93c12ff
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-04-26 18:28:16 -0300

    Remove line after comments

--- examples/fila-de-trabalhos/fila-de-trabalhos.go
+++ examples/fila-de-trabalhos/fila-de-trabalhos.go
@@ -1,6 +1,5 @@
 // Neste exemplo, veremos como implementar uma fila
 // de trabalhas usando goroutines e canais.
-
 package main
 
 import (
@@ -13,7 +12,6 @@ import (
 // trabalho no canal `jobs` e enviarão os resultados
 // correspondentes em `results`. Aguardaremos um segundo
 // por trabalho para simular uma tarefa dispendiosa.
-
 func worker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
 		fmt.Println("worker", id, "started  job", j)
@@ -28,14 +26,12 @@ func main() {
 	// Para usar nosso grupo de `workers`, precisamos 
 	// enviar o trabalho e coletar seus resultados. Criamos
 	// 2 canais para isso.
-
 	const numJobs = 5
 	jobs := make(chan int, numJobs)
 	results := make(chan int, numJobs)
 
 	// Isso inicia 3 `workers`, inicialmente bloqueados
 	// porque ainda não há nenhum `job`.
-
 	for w := 1; w <= 3; w++ {
 		go worker(w, jobs, results)
 	}
@@ -43,7 +39,6 @@ func main() {
 	// Aqui enviamos 5 `jobs` e depois fechamos esse 
 	// canal para indicar que esse é todo o trabalho
 	// que temos.
-
 	for j := 1; j <= numJobs; j++ {
 		jobs <- j
 	}
@@ -53,8 +48,8 @@ func main() {
 	// trabalho. Isso também garante que as rotinas do
 	// `worker` terminou. Uma maneira alternativa de
 	// esperar por várias goroutines é usar um WaitGroup.
-	
 	for a := 1; a <= numJobs; a++ {
 		<-results
 	}
 }
+
