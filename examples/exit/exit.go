574495983ed0e0a17b3bc53b407e9df5df1397af
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-05-09 18:19:00 -0300

    Traduzido exit

--- examples/exit/exit.go
+++ examples/exit/exit.go
@@ -1,5 +1,5 @@
-// Use `os.Exit` to immediately exit with a given
-// status.
+// Use `os.Exit` para sair imediatamente com um status 
+// determinado.
 
 package main
 
@@ -10,15 +10,15 @@ import (
 
 func main() {
 
-	// `defer`s will _not_ be run when using `os.Exit`, so
-	// this `fmt.Println` will never be called.
+	// `defer` não será executado ao usar `os.Exit`, 
+	// então esse `fmt.Println` nunca será chamado.
 	defer fmt.Println("!")
 
-	// Exit with status 3.
+	// Sair com status 3
 	os.Exit(3)
 }
 
-// Note that unlike e.g. C, Go does not use an integer
-// return value from `main` to indicate exit status. If
-// you'd like to exit with a non-zero status you should
-// use `os.Exit`.
+// Observe que, em Go não se usa um valor de retorno 
+// inteiro da função `main` para indicar o status de
+// saída. Se você quiser sair com um status diferente
+// de zero, você deve usar `os.Exit`.
--- examples/exit/exit.sh
+++ examples/exit/exit.sh
@@ -1,13 +1,13 @@
-#  If you run `exit.go` using `go run`, the exit
-# will be picked up by `go` and printed.
+# Se você executar `exit.go` usando `go run`, a 
+# saída será retirada por `go` e exibida.
 $ go run exit.go
 exit status 3
 
-# By building and executing a binary you can see
-# the status in the terminal.
+# Ao criar e executar um binário, você pode ver o
+# status no terminal.
 $ go build exit.go
 $ ./exit
 $ echo $?
 3
 
-# Note that the `!` from our program never got printed.
+# Observe que o `!` do nosso programa nunca foi exibido.
