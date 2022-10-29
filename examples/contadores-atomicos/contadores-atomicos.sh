0ee89f36cc5b5e028b08ab49bfd018c9270516f5
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-10-29 15:28:09 -0300

    Update size line
    Signed-off-by: Mr. Erlison <sisal.cordame_0a@icloud.com>

--- examples/contadores-atomicos/contadores-atomicos.sh
+++ examples/contadores-atomicos/contadores-atomicos.sh
@@ -1,8 +1,8 @@
-# Esperamos obter exatamente 50.000 operações. Se usássemos o 
-# não atômico `ops++` para incrementar o contador, provavelmente
-# teríamos um número diferente, mudando entre execuções, 
-# porque as gorotinas interfeririam umas com as outras. 
-# Além disso, obteriamos falhas de dados na execução
+# Esperamos obter exatamente 50.000 operações. Se usarmos 
+# o não atômico `ops++` para incrementar o contador, 
+# provavelmente teríamos um número diferente, mudando entre 
+# execuções, porque as gorotinas interfeririam umas com as 
+# outras. Além disso, obteriamos falhas de dados na execução
 # com a flag `-race`.
 $ go run contadores-atomicos.go
 ops: 50000
